095adafb5ec89fece8d2adce0ca3967143f22e76
nidhig631 <nidhig631@gmail.com>
2021-08-16 19:23:06 +0530

    add all

---
+++ Python_Projects/_145_Python_Projects/Project1/app.py
@@ -0,0 +1,8 @@
+# Create Font Art using Python
+#  The PyFiglet library in Python can be used to visualize the output of your Python program with an amazing font style.
+# Step:1
+# pip install pyfiglet
+
+import pyfiglet
+font = pyfiglet.figlet_format('Nidhi Gupta')
+print(font)
---
+++ Python_Projects/_145_Python_Projects/Project2/app.py
@@ -0,0 +1,7 @@
+# Collage Maker using Python
+
+from PIL import Image
+import numpy as np
+def collage_maker(image1,image2, name):
+  i1 = np.array(image1)
+  i2 =
